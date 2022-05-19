b667578420046bdf985ef8e0c292cea9b92f9e45
Domingos Neto <84483063+domingosscneto@users.noreply.github.com>
2022-05-18 22:48:08 -0300

    Update main.py

--- PROJ1 V1/main.py
+++ PROJ1 V1/main.py
@@ -5,29 +5,32 @@
 import concurrent.futures
 
 def taylor(x):
-    array = []
     soma = 0
     soma2 = 0
+    soma3 = 0
     total = 0
     if x == 0:
-      for i in range(51):
+      for i in range(500000001):
         if i == 0:
           pass
         else:
           soma += 1/i
     elif x == 1:
-        for j in range(51, 101):
+        for j in range(500000001, 750000001):
           soma2 += 1/j
-    total = soma + soma2
+    elif x == 2:
+        for j in range(750000001, 1000000001):
+          soma3 += 1/j
+    total = soma + soma2 + soma3
     return total
 
 
-with concurrent.futures.ThreadPoolExecutor(2) as executor: #A declaração with é usada para criar um executor de instância do ThreadPoolExecutor que irá esvaziar os threads imediatamente após a conclusão.
+with concurrent.futures.ThreadPoolExecutor(3) as executor: #A declaração with é usada para criar um executor de instância do ThreadPoolExecutor que irá esvaziar os threads imediatamente após a conclusão.
     futures = []
-    for x in range(2):
+    for x in range(3):
         futures.append(executor.submit(taylor, x))#Cada chamada a submit retorna uma instância Future que está armazenada na lista futures.
     somaArr = []
     for future in concurrent.futures.as_completed(futures): #Espera cada chamada da função ser concluída para poder imprimir o resultado.
         somaArr.append(future.result())
-    sum = somaArr[0] + somaArr[1]
-    print(sum)
+    sum = somaArr[0] + somaArr[1] + somaArr[2]
+    print("Sério de Taylor paralelizada com {} Threads = {}".format( 3,sum))
